f61c2a4ae25632974b341a3b7ab95617401e0803
leticiaSouzaDev <lec667121@gmail.com>
2024-11-19 16:01:06 -0300

    feat: adicionando estilização ao componente de Post

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -3,7 +3,7 @@ import styles from './Post.module.css'
 export function Post(){
     return(
         <article className={styles.post}>
-            <header>
+            <header className={styles.cabecalho}>
                 <div className={styles.author}>
                     <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/119368474?v=4" alt="" />      
 
@@ -13,19 +13,23 @@ export function Post(){
                     </div>
                 </div>
 
-                <time dateTime="2024-11-19">Publicado há 1h</time> /*Tag para tempo */
-
+                <time dateTime="2024-11-19">Publicado há 1h</time> 
+            </header>
                 <div className={styles.content}>
 
                      <p>Fala galeraa 👋 </p>
 
                      <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
 
-                     <p>👉 <a href="#">jane.design/doctorcare</a> </p>
+                     <p>👉 {' '} <a href="#">jane.design/doctorcare</a> </p>
 
-                     <p> <a href="#">#novoprojeto #nlw #rocketseat </a> </p>
+                     <p> 
+                        <a href="#">#novoprojeto</a> {' '}
+                        <a href="">#nlw </a> {' '}
+                        <a href=""> #rocketseat</a>
+                    </p>
                 </div>
-            </header>
+            
         </article>
     )
 }
